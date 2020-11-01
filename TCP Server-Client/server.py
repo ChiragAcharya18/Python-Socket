3d9ca10637f915871bd0596f07dbd45604520be0
ChiragAcharya18 <69638126+ChiragAcharya18@users.noreply.github.com>
2020-11-01 15:21:40 +0530

    Create Server.py

---
+++ TCP Server-Client/server.py
@@ -0,0 +1,23 @@
+import socket
+import sys
+
+s = socket.socket()
+print("Socket Created")
+name = socket.gethostname()
+host = socket.gethostbyname(name) #or host = localhost
+print("Host: ",host)
+s.bind((host, 9999))
+s.listen(3)
+print("Waiting for connection...")
+
+
+while True:
+    c, add = s.accept()
+    msg = c.recv(1024).decode()
+    if msg == "free" or not msg:
+        print("Disconnecting... ")
+        break
+    print(f"Connected with IP: {add[0]} Port: {add[1]} \nMessage: {msg}")
+    c.send(bytes("Welcome!", 'utf-8'))
+
+s.close()
